13fa7d93c812e5b8dec9b0ec503862ff8379f313
fuhao <fuhaojun@52tt.com>
2026-01-17 15:50:32 +0800

    nest IoC机制

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Inject(AppService)
+  // private appService: AppService;
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  //   @Inject(OrderService)
+  //   private orderService: OrderService;
+
+  //   @Inject(EmployeeService)
+  //   private employeeService: EmployeeService;
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ModuleAModule } from './module-a/module-a.module';
+import { ServiceAService } from './service-a/service-a.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ModuleAModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ServiceAService],
 })
 export class AppModule {}
